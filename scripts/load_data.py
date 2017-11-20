bfa53677ba2d9f35bed22524cc432e60da179e53
Max Spero <mmspero@gmail.com>
2017-11-19 16:23:24 -0800

    update cifar10 to cifar100

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -8,14 +8,14 @@ def get_transform():
 
 def load_train_data():
     transform = get_transform()
-    trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, 
+    trainset = torchvision.datasets.CIFAR100(root='./data', train=True, download=True, 
                                             transform=transform)
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=32, shuffle=True, num_workers=4)
     return trainloader
 
 def load_test_data():    
     transform = get_transform()
-    testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, 
+    testset = torchvision.datasets.CIFAR100(root='./data', train=False, download=True, 
                                             transform=transform)
     testloader = torch.utils.data.DataLoader(testset, batch_size=32, shuffle=False, num_workers=4)
     return testloader
